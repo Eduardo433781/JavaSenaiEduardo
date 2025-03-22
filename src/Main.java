ef3c5743858fbbcd4e77ca37bb80cd76e2dc29c8
Eduardo433781 <eduardo.coerse@gmail.com>
2025-03-22 10:17:08 -0300

    feat: AulaJava

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num = 10;
+        Long num4 = 10L;
+
+        //------------------------------------------
+        String nome = "Eduardo";
+        System.out.println("Meu nome é: " + nome);
+
+        //------------------------------------------
+        float num2 = 10.5f;
+        double num3 = 10.5;
+
+        char valor = 129;
+
+        System.out.println(num + num4);
+        System.out.println(num - num4);
+        System.out.println(num * num4);
+        System.out.println(num / num4);
+        System.out.println(num % num4);
+
+        //-------------------------------------------
+        byte newNovo = 127;
+        byte newNovo2 = -128;
+        System.out.println(newNovo);
+        System.out.println(newNovo2);
+
+        //-------------------------------------------
+        char status = 'M';
+        char status1 = 'A';
+        char status2 = 'R';
+        System.out.println(status);
+        System.out.println(status1);
+        System.out.println(status2);
+
+
+
+
+
+
+
+
+
+
+    }
+}
